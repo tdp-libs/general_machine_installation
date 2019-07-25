51a3adc6270a0ddc4cbb793320732127d19fe421
Tom Paynter <thomas.paynter@diditalbarriers.com>
2019-07-25 13:27:51 +0100

    Added XFCE+KDE desktops, NVIDIA deps, and VS Code to main install.

--- Fedora/install.sh
+++ Fedora/install.sh
@@ -2,7 +2,7 @@
 
 #An installation script for install the tools common to a Development Machine
 
-set -e
+set -xe
 
 if id "$1" >/dev/null 2>&1; then
   echo "Installing for user: $1"
@@ -28,6 +28,10 @@ dnf install -y "perl(Data::Dumper)" "perl(Thread::Queue)"
 dnf install -y mesa-libGLES-devel mesa-libEGL-devel
 dnf install -y gperftools-libs
 
+#Required for NVIDIA in some situations
+dnf install kernel-devel kernel-headers gcc make dkms acpid libglvnd-glx libglvnd-opengl libglvnd-devel pkgconfig
+
+
 #Install RPM fusion
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
@@ -41,3 +45,15 @@ su - $1 -c "git config --global core.excludesfile ~/.gitignore"
 
 #Graphics & office software
 dnf install -y inkscape abiword kolourpaint
+
+#It seems to make more sense to use the Gnome spin and then install XFCE
+dnf groupinstall -y "Xfce Desktop"
+#And KDE for to get Dolphin
+dnf groupinstall -y "KDE Plasma Workspaces"
+
+#VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf check-update
+dnf install -y code
