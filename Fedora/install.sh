55c5132f8132fd4f1dacad2124fd20487a2b2154
Tom Paynter <thomas.paynter@diditalbarriers.com>
2019-11-20 09:47:36 +0000

    Small fix.

--- Fedora/install.sh
+++ Fedora/install.sh
@@ -56,7 +56,7 @@ dnf groupinstall -y "KDE Plasma Workspaces"
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-dnf check-update
+dnf check-update || true
 dnf install -y code
 
 # Video, VLC, ffmpeg, OpenCV 
